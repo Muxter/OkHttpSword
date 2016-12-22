cee0bfb7e50f07e35d792367fdcaf2a8fcfd1337
matao <matao.mardle@gmail.com>
2016-12-22 10:39:14 +0800

    [ADD] Add method file

---
+++ okhttpsword/src/main/java/com/matao/okhttpsword/utils/Method.java
@@ -0,0 +1,12 @@
+package com.matao.okhttpsword.utils;
+
+/**
+ * Created by matao
+ */
+
+public class Method {
+    public static final String HEAD = "HEAD";
+    public static final String DELETE = "DELETE";
+    public static final String PUT = "PUT";
+    public static final String PATCH = "PATCH";
+}
